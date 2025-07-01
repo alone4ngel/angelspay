cd7b43092de96fab63c6dfc3df74329875a4c635
alone4ngel <94761962+alone4ngel@users.noreply.github.com>
2025-07-02 01:36:36 +0800

    Update useBalance.ts
    5kk

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 export const useBalance = () => {
   const [balance, setBalance] = useState(() => {
     const savedBalance = localStorage.getItem('balance');
-    return savedBalance ? Number(savedBalance) : 4700444;
+    return savedBalance ? Number(savedBalance) : 5700444;
   });
 
   useEffect(() => {
